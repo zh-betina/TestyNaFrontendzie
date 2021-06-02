a3a49ddad3315bc749345392e48c24b0d7769efc
pSiemieniuk <psiemieniuk@thebrain.pro>
2021-06-02 09:57:29 +0200

    remove loader from home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import { Products } from "../organisms/Products";
 import Container from "../templates/Container";
 import ListHeader from "../atoms/ListHeader";
 import { Header } from "../organisms/Header";
-import Loader from "../atoms/Loader";
 
 export const Home = (): JSX.Element => {
   return (
