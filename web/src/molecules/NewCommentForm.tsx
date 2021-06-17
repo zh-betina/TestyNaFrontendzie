cf78773a253b544da42637d3427e2f1219931b52
pSiemieniuk <psiemieniuk@thebrain.pro>
2021-06-17 19:13:16 +0200

    Fix NewCommentForm test  - add i18nForTests

--- web/src/molecules/NewCommentForm.tsx
+++ web/src/molecules/NewCommentForm.tsx
@@ -32,7 +32,7 @@ export const NewCommentForm = ({ submit }: Props) => {
       <FormInputs>
         <div>
           <label htmlFor="userName">
-            <span>{t("Your name")}:</span>
+            <span>{t("labelYourName")}:</span>
             <input
               id="userName"
               type="text"
