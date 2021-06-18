a0407b817b75acac1e8f38a5d569ec2b5edbb40e
pSiemieniuk <psiemieniuk@thebrain.pro>
2021-06-18 09:24:51 +0200

    test

--- web/src/organisms/__tests__/Comments.jsx
+++ web/src/organisms/__tests__/Comments.jsx
@@ -1,30 +1,29 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import user from "@testing-library/user-event";
-import { Comments } from "../Comments";
+import React from 'react';
+import {render, screen} from "@testing-library/react";
+import user from "@testing-library/user-event"
+import {Comments} from "../Comments";
 
-describe("<Comments/>", () => {
-  it("shows a form to add a new comment when user click on button for open the form", () => {
-    render(<Comments productId="prod-id-1" />);
-    const yourNameLabelText = /twoje imię/i;
+describe('<Comments/>', () => {
+  it('shows a form to add a new comment when user click on button for open the form', () => {
+    render(<Comments productId={"prod-id"}/>)
 
-    expect(screen.queryByLabelText(yourNameLabelText)).toBeNull();
+    expect(screen.queryByLabelText(/twoje imię/i)).toBeNull();
 
-    const openAddNewCommentFormButton =
-      screen.getByText(/dodaj nowy komentarz/i);
-    user.click(openAddNewCommentFormButton);
+    const addNewCommentButton = screen.getByRole('button', {name: /dodaj nowy komentarz/i})
+    user.click(addNewCommentButton);
 
-    expect(screen.getByLabelText(yourNameLabelText)).toBeDefined();
+    expect(screen.getByLabelText(/twoje imię/i));
   });
 
-  it("hides a form to add a new comment after user click after cancel button", () => {
-    render(<Comments productId="prod-id-1" />);
-    const yourNameLabelText = /twoje imię/i;
-    user.click(screen.getByText(/dodaj nowy komentarz/i));
+  it('hides a form to add a new comment after user click cancel button', () => {
+    render(<Comments productId={"prod-id"}/>)
 
-    const cancelButton = screen.getByText(/anuluj/i);
+    const addNewCommentButton = screen.getByRole('button', {name: /dodaj nowy komentarz/i})
+    user.click(addNewCommentButton);
+
+    const cancelButton = screen.getByRole('button', {name: /anuluj/i})
     user.click(cancelButton);
 
-    expect(screen.queryByLabelText(yourNameLabelText)).toBeNull();
+    expect(screen.queryByLabelText(/twoje imię/i)).toBeNull();
   });
 });
