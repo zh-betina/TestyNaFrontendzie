f81f907e6c6ff7ecbebba77a1f87a27f5dfc59a2
Anna Konopka <kernelgonnapanic@gmail.com>
2021-06-19 11:31:11 +0200

    M3L8

--- web/src/testsUtils/test-utils.tsx
+++ web/src/testsUtils/test-utils.tsx
@@ -1,7 +1,11 @@
 import React, { FC, ReactElement } from "react";
 import { omit } from "lodash";
-import { render, RenderOptions } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+import { render, RenderOptions, RenderResult } from "@testing-library/react";
 import { I18nextProvider } from "react-i18next";
+import { RootState, reducer } from "../state/store";
 import {
   CurrencyContextInterface,
   CurrencyProvider,
@@ -11,22 +15,34 @@ import { startingState } from "../currencyContext/reducers";
 
 type AllTheProvidersProps = {
   value?: CurrencyContextInterface;
+  store?: ReturnType<typeof getTestStore>;
+};
+
+export const getTestStore = (preloadedState?: Partial<RootState>) => {
+  return configureStore({
+    reducer,
+    preloadedState,
+  });
 };
 
 const customRender = (
   ui: ReactElement,
   options?: Omit<RenderOptions, "queries"> & AllTheProvidersProps
-) => {
+): RenderResult => {
   let value = { state: startingState };
   if (options && options.value) {
     value = options.value;
   }
 
+  const testStore = options?.store ?? getTestStore();
+
   const AllTheProviders: FC = ({ children }) => {
     return (
-      <CurrencyProvider value={value}>
-        <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
-      </CurrencyProvider>
+      <Provider store={testStore}>
+        <CurrencyProvider value={value}>
+          <I18nextProvider i18n={i18n}>{children}</I18nextProvider>
+        </CurrencyProvider>
+      </Provider>
     );
   };
 
