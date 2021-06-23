8cd3434a07cc8a8214dbf31751a3cdfb121c844a
pSiemieniuk <psiemieniuk@thebrain.pro>
2021-06-24 00:04:38 +0200

    Update getProducts in server

--- server/src/mockData/getProducts.js
+++ server/src/mockData/getProducts.js
@@ -1,21 +1,133 @@
 const getProducts = () => {
   return [
     {
-      name: 'Buty sportowe',
+      name: {
+        pl: 'Buty sportowe',
+        en: 'Sports shoes',
+      },
       brand: 'Adidas',
-      price: 55000,
+      price: [
+        {
+          currency: 'PLN',
+          price: 25000,
+        },
+        {
+          currency: 'USD',
+          price: 6688,
+        },
+        {
+          currency: 'EUR',
+          price: 5517,
+        },
+      ],
     },
     {
-      name: 'Koszulka sportowa',
+      name: {
+        pl: 'Koszulka sportowa',
+        en: 'Sports T-shirt',
+      },
       brand: 'Nike',
-      price: 5000,
+      price: [
+        {
+          currency: 'PLN',
+          price: 5000,
+        },
+        {
+          currency: 'USD',
+          price: 1338,
+        },
+        {
+          currency: 'EUR',
+          price: 1104,
+        },
+      ],
     },
     {
-      name: 'Spodnie dresowe',
+      name: {
+        pl: 'Buty trekingowe',
+        en: 'Trekking boots',
+      },
+      brand: 'HiTec',
+      price: [
+        {
+          currency: 'PLN',
+          price: 8900,
+        },
+        {
+          currency: 'USD',
+          price: 2381,
+        },
+        {
+          currency: 'EUR',
+          price: 1964,
+        },
+      ],
+    },
+    {
+      name: {
+        pl: 'Spodnie dresowe',
+        en: 'Sweatpants',
+      },
       brand: 'Reebok',
-      price: 8900,
+      price: [
+        {
+          currency: 'PLN',
+          price: 8900,
+        },
+        {
+          currency: 'USD',
+          price: 2381,
+        },
+        {
+          currency: 'EUR',
+          price: 1964,
+        },
+      ],
+    },
+    {
+      name: {
+        pl: 'Spodnie dresowe',
+        en: 'Sweatpants',
+      },
+      brand: 'Adidas',
+      price: [
+        {
+          currency: 'PLN',
+          price: 6000,
+        },
+        {
+          currency: 'USD',
+          price: 1605,
+        },
+        {
+          currency: 'EUR',
+          price: 1324,
+        },
+      ],
+    },
+    {
+      name: {
+        pl: 'Spodnie dresowe',
+        en: 'Sweatpants',
+      },
+      brand: 'Nike',
+      price: [
+        {
+          currency: 'PLN',
+          price: 25000,
+        },
+        {
+          currency: 'USD',
+          price: 6688,
+        },
+        {
+          currency: 'EUR',
+          price: 5517,
+        },
+      ],
     },
   ];
 };
+
 
 module.exports = getProducts;
