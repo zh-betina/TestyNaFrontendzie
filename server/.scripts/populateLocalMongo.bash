27153f18fbb126910624fbec9d78606c6ae3ad4b
pSiemieniuk <psiemieniuk@thebrain.pro>
2021-06-26 22:31:32 +0200

    Fix problem with json files

--- server/.scripts/populateLocalMongo.bash
+++ server/.scripts/populateLocalMongo.bash
@@ -1,2 +1,2 @@
-docker exec -i tnf-mongo sh -c "mongoimport -c Products -d tnf-server --drop" < .scripts/data/Products.json
-docker exec -i tnf-mongo sh -c "mongoimport -c Comments -d tnf-server --drop" < .scripts/data/Comments.json
+docker exec -i tnf-mongo sh -c "mongoimport -c Products -d tnf-server --drop" < .scripts/Products.json
+docker exec -i tnf-mongo sh -c "mongoimport -c Comments -d tnf-server --drop" < .scripts/Comments.json
